b833ada79b43039ec7839bcc7db91b6a26a46f67
Manju89-stackly <manjunadhg@thestackly.com>
2026-06-27 15:07:03 +0530

    task2

---
+++ Jscript/task2/script.js
@@ -0,0 +1,9 @@
+// Task 2 - Company Entry Confirmation
+// Show a confirm box:
+let userAnswer = confirm("Are you ready to join today's JavaScript training?");
+
+// 1. Print the user's answer (true/false) in the console:
+console.log(userAnswer);
+
+// 2. Print the user's answer on the HTML page using writeln:
+document.writeln("Answer: " + userAnswer);
